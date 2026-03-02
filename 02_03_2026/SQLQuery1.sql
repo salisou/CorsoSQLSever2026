91262b4cb7433a595cc4331dfc48b624ae0d4ede
Moussa <salisoumoussa1@gmail.com>
2026-03-02 13:01:26 +0100

    Aggiungere i file di progetto.

---
+++ 02_03_2026/SQLQuery1.sql
@@ -0,0 +1,91 @@
+-- Creazionde del Database
+Create Database _ScuolaDb
+
+
+-- Uso o attivazion del database
+use _ScuolaDb
+
+
+-- Creazione della Tabella Studenti con i iti di dati
+/*
+	int = intero
+	varchar = string o Text
+	nvarchar = string o Text
+	char = carattere 
+	decimal
+	bit
+	numeric
+	foaltreal
+	date
+	time
+	datetime2 
+	datetimeoffset
+	smaldatetime
+*/
+
+
+/*====================================================
+//
+//
+//
+======================================================*/
+
+Create Table Studente
+(
+	StudenteId int primary key identity(1,1) NOT NULL, 
+	Nome NVARCHAR(100) NOT NULL,
+	Cognome NVARCHAR(100) NOT NULL,
+	DataNascita Date NULL,
+	Genere CHAR(1), 
+	CodiceFiscale NVARCHAR(250)
+);
+
+-- Selezionare tutti i record della Tabella Studente
+Select * From Studente
+
+
+--Inserimento dei dati nella tabella studente
+INSERT INTO Studente 
+(Nome, Cognome, DataNascita, Genere, CodiceFiscale) 
+VALUES 
+('Prisca', 'Malba', '1985-02-10', 'F', 'PRMABC85B50H501X'),
+('Luca', 'Rossi', '2000-05-15', 'M', 'RSSLCU00E15H501Y'),
+('Maria', 'Bianchi', '1998-11-22', 'F', 'BNCMRA98S62H501Z'),
+('Giovanni', 'Verdi', '1995-07-30', 'M', 'VRDGNN95L30H501A'),
+('Sara', 'Conti', '2001-03-18', 'F', 'CNTSRA01C58H501B'),
+('Marco', 'Ferrari', '1997-09-09', 'M', 'FRRMRC97P09H501C'),
+('Elena', 'Romano', '1999-12-01', 'F', 'RMNLNE99T41H501D'),
+('Davide', 'Ricci', '1996-04-25', 'M', 'RCCDVD96D25H501E'),
+('Chiara', 'Gallo', '2002-08-14', 'F', 'GLLCHR02M54H501F'),
+('Matteo', 'Costa', '1994-01-05', 'M', 'CSTMTT94A05H501G');
+
+--Modifica il tipo della colonna (CodiceFiscale NVARCHAR(250) => Char(16))
+--ALTER TABLE Studente
+--ALTER COLUMN CodiceFiscale CHAR(16);
+-- Definisce il valore fissa della colonna (Genere CHAR(1))
+ALTER TABLE Studente
+Add Constraint CK_Studente_Genere
+CHECK(Genere IN ('M', 'F') AND Genere IS NOT NULL)
+
+
+/*
+Visualizzazione e filtro dati
+SELECT visualizza i dati inseriti e 
+WHERE filtra i risultati secondo criteri specifici.
+
+=, Like, <,>, !=, And, OR, IN, NOT IN, IF, IF NOT, IF NOT EXISTS
+*/
+
+Select * From Studente where StudenteId = 7
+Select * From Studente WHERE Genere = 'F'
+Select * From Studente Where Genere = 'M'
+Select * From Studente Where Cognome LIKE 'R%'
+Select * From Studente Where Cognome LIKE 'R%' AND Cognome = 'Romano'
+Select * From Studente WHERE Genere = 'F' and DataNascita is not null
+
+
+Select Nome, CodiceFiscale from Studente order by Nome Desc
+Select StudenteId , CodiceFiscale,  Nome, Cognome, DataNascita, Genere  from Studente order by Nome Desc
+
+-- Restiture l'elecnco degli studenti che sono nati prima dell'anno 2000
+-- Mostra solo gli studente maschi nati dopo anno 1996
